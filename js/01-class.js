bf4cce31e13ed1611b40c97c64f1dff8c8a286b8
Julia Lozenko <jlozenko@gmail.com>
2021-03-24 12:36:02 +0200

    test

--- js/01-class.js
+++ js/01-class.js
@@ -9,48 +9,72 @@
  * 🐷 - геттеры и сеттеры
  */
 
+// class Car {
+//     static description = 'Класс описывающий автомобиль';
+
+//     static logInfo(carObj) {
+//         console.log('Car.logInfo -> carObj', carObj);
+//     }
+
+//     constructor({ brand, model, price } = {}) {
+//         this.brand = brand;
+//         this._model = model;
+//         this._price = price;
+//     }
+
+//     get price() {
+//         return this._price;
+//     }
+
+//     set price(newPrice) {
+//         this._price = newPrice;
+//     }
+
+//     get model() {
+//         return this._model;
+//     }
+
+//     set model(newModel) {
+//         this._model = newModel;
+//     }
+// }
+
+// const carInstance = new Car({
+//     brand: 'Audi',
+//     model: 'Q3',
+//     price: 35000,
+// });
+
+// console.log(carInstance.model);
+// carInstance.model = 'Q4';
+// console.log(carInstance.model);
+
+// console.log(carInstance.price);
+// carInstance.price = 50000;
+// console.log(carInstance.price);
+
+// console.log(carInstance);
+
+
 class Car {
-    static description = 'Класс описывающий автомобиль';
+  static #MAX_PRICE = 50000;
+  // Пиши код ниже этой строки
 
-    static logInfo(carObj) {
-        console.log('Car.logInfo -> carObj', carObj);
-    }
-
-    constructor({ brand, model, price } = {}) {
-        this.brand = brand;
-        this._model = model;
-        this._price = price;
-    }
-
-    get price() {
-        return this._price;
-    }
-
-    set price(newPrice) {
-        this._price = newPrice;
-    }
-
-    get model() {
-        return this._model;
-    }
-
-    set model(newModel) {
-        this._model = newModel;
-    }
+  static checkPrice(price){
+    if (price > Car.#MAX_PRICE){
+      return `Внимание! Цена превышает допустимую.`;
+    } 
+      return `Всё хорошо, цена в порядке.`;
 }
 
-const carInstance = new Car({
-    brand: 'Audi',
-    model: 'Q3',
-    price: 35000,
-});
+  // Пиши код выше этой строки
+  constructor({ price }) {
+    this.price = price;
+  }
+}
 
-console.log(carInstance.model);
-carInstance.model = 'Q4';
-console.log(carInstance.model);
+const audi = new Car({ price: 36000 });
+const bmw = new Car({ price: 64000 });
 
-console.log(carInstance.price);
-carInstance.price = 50000;
-console.log(carInstance.price);
-
-console.log(carInstance);
+console.log(Car.checkPrice(audi.price)); // Всё хорошо, цена в порядке.
+console.log(Car.checkPrice(bmw.price)); // Внимание! Цена превышает допустимую.
